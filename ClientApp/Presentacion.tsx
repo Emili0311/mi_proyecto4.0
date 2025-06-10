9b00f0b142b376ec3ca6fcc73aa87f77ea1f2049
Emili0311 <emiliyoselyn.gutierrezsalas.gen23@cbtis105.edu.mx>
2025-06-10 10:07:39 -0600

    lodimostodo

---
+++ ClientApp/Presentacion.tsx
@@ -0,0 +1,15 @@
+const Presentacion = () => {
+     // Manejo de datos 
+
+     // Vista
+    return{
+     <>
+         <div className="display-4">Nombre del cbtis</div>
+          <div className="h1">Integrantes</div>
+          <div className="h2">Nombre del integrante1</div>
+          <div className="h2">Nombre del integrante2</div>
+          <div className="h1">Nombre del Proyecto</div>
+     </>
+     }
+}
+export default Presentacion;
